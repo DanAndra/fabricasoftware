1e0b01df6d4ae6c8d5a36a54c951094c5023b294
danandra <dpafunipe@gmail.com>
2023-08-28 17:04:20 -0300

    Enviando a primeira atividade

---
+++ atvd01.py
@@ -0,0 +1,7 @@
+"""
+1. crie um programa que peça um numero e mostre o seu sucessor e antecessor
+"""
+
+num = int(input("digite um numero para ver seu sucessor e antecessor: "))
+print("seu antecessor: ", num - 1)
+print("seu sucessor: ", num + 1)
